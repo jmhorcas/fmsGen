e62934300aea175b274d54081506796b2a54d69c
José Miguel Horcas <horcas@uma.es>
2024-03-08 12:55:48 +0100

    Clean code

--- app/blueprints/config_gen/routes.py
+++ app/blueprints/config_gen/routes.py
@@ -1,5 +1,4 @@
 from typing import Any 
-import shutil
 
 import flask
 from celery.result import AsyncResult
@@ -25,10 +24,10 @@ def update_progress(task_id) -> dict[str, object]:
             return flask.Response(data, mimetype="text/event-stream")
         else:
             # Task completed with an error
-            # flash message with error
-            return flask.url_for('index')
+            flask.flash(f"The generation task has completed with an error.", category='error')
+            return flask.redirect(flask.url_for('config_gen.index'))
     elif task.status == 'PENDING':
-        data = 'data: 100\n\n'
+        data = 'data: 0\n\n'
         return flask.Response(data, mimetype="text/event-stream")
     elif task.status == 'PROGRESS':
         percentage_completed = int((task.info['current'] / task.info['total']) * 100)
@@ -37,69 +36,33 @@ def update_progress(task_id) -> dict[str, object]:
 
 @config_gen_bp.route("/get_result/<task_id>", methods=['GET'])
 def task_result(task_id) -> dict[str, object]:
-    print(f'/get_result')
-    print(f'/result_id {task_id}')
-    result = AsyncResult(task_id)
-    if result.ready():
-        print(f'result.ready')
+    task = AsyncResult(task_id)
+    if task.ready():
         # Task has completed
-        if result.successful():
-            params = result.result
+        if task.successful():
+            params = task.result
             temp_zipfile = params[Params.ZIP_FILE.name]['value']
             zip_filename = params[Params.ZIP_FILENAME.name]['value']
-            print(f'zip.file: {temp_zipfile}')
-            print(f'zip.zip_filename: {zip_filename}')
-            print(f'SERIALIZATION_TEMPORAL_DIR: {params[Params.SERIALIZATION_TEMPORAL_DIR.name]["value"]}')
             response = flask.make_response(flask.send_file(path_or_file=temp_zipfile, 
                                                            as_attachment=True, 
                                                            download_name=zip_filename))
             return response
         else:
             # Task completed with an error
-            #sse.publish({"message": "Hello!"}, type='greeting')
-            return flask.jsonify({'status': 'ERROR', 'error_message': str(result.result)})
+            flask.flash(f"The generation task has completed with an error.", category='error')
+            return flask.redirect(flask.url_for('config_gen.index'))
     else:
         # Task completed with an error
-        #sse.publish({"message": "Hello!"}, type='greeting')
-        return flask.jsonify({'status': 'ERROR', 'error_message': str(result.result)})
+        flask.flash(f"Error in the generation task.", category='error')
+        return flask.redirect(flask.url_for('config_gen.index'))
     
 
-# def generate():
-#     x = 0
-#     while x <= 100:
-#         yield "event: progress_bar\ndata:" + str(x) + "\n\n"
-#         x = x + 10
-#         time.sleep(0.5)
-
-
-# def stream_template(template_name, **context):
-#     flask.current_app.update_template_context(context)
-#     t = flask.current_app.jinja_env.get_template(template_name)
-#     rv = t.stream(context)
-#     rv.enable_buffering(5)
-#     return rv
-
-# @config_gen_bp.route('/progress')
-# def progress():
-#     if flask.request.method == 'GET':    
-#         #data = generate_feature_models(params)
-#         #return flask.current_app.response_class(stream_template('config_gen/index.html', data=data), mimetype='text/event-stream')
-#         return flask.Response(generate(), mimetype='text/event-stream')
-#         #return flask.Response(generate_feature_models(params), mimetype='text/event-stream')
-
-
-@config_gen_bp.route('/fmsGen', methods=['POST'])
+@config_gen_bp.route('/', methods=['POST'])
 def fms_gen():
     request = flask.request
     if request.method == 'POST':
         params = get_parameters_from_request(request)
-        #return flask.Response(generate(), mimetype='text/event-stream')
-        # Generate random feature models
-        #params[Params.SERIALIZATION_TEMPORAL_DIR.name] = Params.SERIALIZATION_TEMPORAL_DIR.value.to_dict()
-        #params[Params.SERIALIZATION_TEMPORAL_DIR.name]['value'] = temp_dir
-        print(f'Generate feature models...')
         result = generate_feature_models.delay(params)
-        print(f'Generate feature models (done!)')
         params[Params.TASK_ID.name]['value'] = result.id
         return flask.render_template('config_gen/index.html', params=params)
 
--- app/blueprints/config_gen/static/js/progress_bar.js
+++ app/blueprints/config_gen/static/js/progress_bar.js
@@ -1,16 +1,3 @@
-// const generateButton = document.getElementById('generate_button');
-// generateButton.onclick = function() {
-// 	var source = new EventSource("/get_result");
-// 	source.onmessage = function(event) {
-// 		$('.progress-bar').css('width', event.data+'%').attr('aria-valuenow', event.data);
-// 		$('.progress-bar-label').text(event.data+'%');
-
-// 		if (event.data == 100) {
-// 			source.close();
-// 		}
-// 	}
-// };
-
 function downloadFile(url, fileName) {
 	fetch(url, { method: "get", mode: "no-cors", referrerPolicy: "no-referrer" })
 	  .then((res) => res.blob())
@@ -22,11 +9,12 @@ function downloadFile(url, fileName) {
 		aElement.setAttribute("target", "_blank");
 		aElement.click();
 		URL.revokeObjectURL(href);
-	  });
-  }
+	});
+}
 
 function update_progress_bar(task_id) {
-	console.log("update progress bar: " + task_id);
+	$("#generate_button").prop("disabled", true);
+	$("#generate_button").text('Generating...');
 	var source = new EventSource("/update_progress/" + task_id);
 	source.onmessage = function(event) {
 		$('.progress-bar').css('width', event.data+'%').attr('aria-valuenow', event.data);
@@ -34,43 +22,12 @@ function update_progress_bar(task_id) {
 
 		if (event.data == 100) {
 			source.close();
-			var result_url = "/get_result/" + task_id
+			var result_url = "/get_result/" + task_id;
 			downloadFile(result_url, 'name.zip');
-			// fetch(result_url).then(function(resp) {
-			// 	return resp.blob();
-			// }).then(function(blob) {
-			// 	download(blob);
-			// });
+			$("#generate_button").prop("disabled", false);
+			$("#generate_button").text('Generate!');
+			//$('.progress-bar').css('width', '0%').attr('aria-valuenow', '0');
+			//$('.progress-bar-label').text('0%');
 		}
 	}
 };
-
-
-// var source = new EventSource("/get_result");
-// 	source.onmessage = function(event) {
-// 		$('.progress-bar').css('width', event.data+'%').attr('aria-valuenow', event.data);
-// 		$('.progress-bar-label').text(event.data+'%');
-
-// 		if (event.data == 100) {
-// 			source.close();
-// 		}
-// 	}
-
-// var eventSource = new EventSource("/progress");
-// eventSource.addEventListener = ('progress_bar', function(event) {
-// 	$('.progress-bar').css('width', event.data+'%').attr('aria-valuenow', event.data);
-// 	$('.progress-bar-label').text(event.data+'%');
-
-// 	if (event.data == 100) {
-// 		eventSource.close();
-// 	}
-// });
-
-// var source = new EventSource("{{ url_for('config_gen.get_result') }}");
-// source.addEventListener('greeting', function(event) {
-// 	var data = JSON.parse(event.data);
-// 	alert("The server says " + data.message);
-// }, false);
-// source.addEventListener('error', function(event) {
-// 	alert("Failed to connect to event stream. Is Redis running?");
-// }, false);
